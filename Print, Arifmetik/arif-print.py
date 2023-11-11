f576b8d430cda4b2d60d1dc154c567e533aadd0f
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-11 17:50:11 +0500

    I Started to learn Python programming language

---
+++ Print, Arifmetik/arif-print.py
@@ -0,0 +1,50 @@
+# TODO Arifmetik amallar
+# TODO Sinteks
+
+print("I want to learn  'Python' progrmming language ")
+
+print('You should write words with "two symbol" inside ')
+
+
+# Hisoblash !!!
+
+# Bo'luv amalida bitta slash('/') nuqatadan keyingi qiymati chiqaradi
+# Sonni yaxlitlash uchun ikkita slash('//') qo'yiladi
+
+###############
+print(20/2)
+print(50//5)
+print (10*3)
+
+##############
+number = 50
+number2 = 30
+print(number + number2)
+
+
+################
+a = 100
+b = 20
+c = 10
+print(a + b * c)
+
+#TODO-Arifmetik amallar !!!!
+######(//) => Bo'lish va butun qismini olish
+######(**) => Sonni darajaga ko'tarish
+######(%) => Bo'linmani qoldig'ini olish
+
+print("To'qqizning kvadrati", 9**2, "ga teng")
+print("Yettining uchinchi darajasi", 7**3, "teng")
+
+###Misol
+x = 14
+y = 25
+output = x**2 + y**2
+print(output / 2) #RESULT => 410.5
+
+# Yoki
+a = 10**2
+b = 25**2
+c = a + b 
+print(c * 2) #Result => 1450
+
