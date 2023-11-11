1d90bf6f8614e71d5bfc1dcb15129da1da011c60
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-11 18:13:46 +0500

    Test

--- Variables/python.py
+++ Variables/python.py
@@ -1,4 +1,13 @@
+
+# O'zgaruvchi —kompyuter xotirasida ma'lum bir qiymatni saqlash uchun ajratilgan joy.
+
+
 a = 10
 b = 20
 result = a + b
 print(result)
+
+myName = "Mehrojbek"
+myFamiliya = "Alieev"
+print(myName)
+
