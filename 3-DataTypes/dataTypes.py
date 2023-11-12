a044e358df99960de920686ee350a34098dc6902
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-12 18:31:21 +0500

    I learned DataTypes of Python

---
+++ 3-DataTypes/dataTypes.py
@@ -0,0 +1,41 @@
+# Data-Types
+# TODO str
+# TODO int
+# TODO float
+# TODO complex
+# TODO list
+# TODO tuple
+# TODO range
+# TODO bool
+# TODO NoneType
+
+
+# Ma'lumot turlarini olish
+# numb = 132
+# words = "Hello World"
+# numbDote = 150000.03
+# sentense = 1j
+# cars = ["Bmw", "Audi", "Dodge", "Bugatti"]
+# fruits = ("apple", "cherry", "pear")
+
+# print(type(numb)) ### => int
+# print(type(words)) ### => str
+# print(type(numbDote)) ### => float
+# print(type(sentense)) ### => complex
+# print(type(cars)) # => list
+# print(type(fruits)) # => tuple
+
+
+
+# Ma'lumot turlarini berish
+
+phoneNumber = int(9989999999)
+print(phoneNumber)
+
+#or
+userId = str(20)
+print(userId)
+print(type(userId)) # => str
+
+
+
