4f6157ed02e2c94c500c24b7c6697fd9be75b09b
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-12 20:14:48 +0500

    Just Practise

--- 3-DataTypes/dataTypesPractise.py
+++ 3-DataTypes/dataTypesPractise.py
@@ -3,3 +3,8 @@ mijoz = int(input("How many bread do you want to buy ? "))
 output = mijoz * cost_bread
 
 print(output)
+
+price = 100
+mijoz = int(input("How much do you need ?"))
+result = (price * mijoz)**2
+print(result)
