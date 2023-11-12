c5e9ca3910a5657d0bb3c4ca7c84cc10df0007c7
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-12 17:12:36 +0500

    learn datatypes

--- DataTypes/dataTypes.py
+++ DataTypes/dataTypes.py
@@ -8,3 +8,12 @@
 # TODO range
 # TODO bool
 # TODO NoneType
+
+numb = 132
+words = "Hello World"
+numbDote = 150000.03
+sentense = "JavaScript have made by America"
+print(type(numb))
+print(type(words))
+print(type(numbDote))
+print(type(sentense))
