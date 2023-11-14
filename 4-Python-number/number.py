2c6fc29030bdf371262737b674c917d7f337723a
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-14 23:29:47 +0500

    Just Test

--- 4-Python-number/number.py
+++ 4-Python-number/number.py
@@ -10,3 +10,21 @@ z = 1j
 print(type(x))
 print(type(y))
 print(type(z))
+
+
+#Float
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Complex
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
