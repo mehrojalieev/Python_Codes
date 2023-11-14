6d1329cba2cc86ac125abc34b5cb307edc1e0f21
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-14 23:25:17 +0500

    Learned type

--- 4-Python-number/number.py
+++ 4-Python-number/number.py
@@ -1,4 +1,12 @@
 # Pythonda uchta raqamli tur mavjud !
-# TODO int
-# TODO float
-# TODO complex
+# TODO int => Butun sonlarni o'z ichiga oladi.
+# TODO float  => Bir yoki bir nechta o'nli kasrlarni o'z ichiga oladi.
+# TODO complex =>  Kompleks sonlar xayoliy qism sifatida "j" bilan yoziladi:
+
+
+x = 10
+y = 55.2
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
