5f6cca53b7ffdfc4775c317c675e92ec2d0622bc
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-15 19:41:42 +0500

    TelegramBot Codes added

--- 3-DataTypes/dataTypesPractise.py
+++ 3-DataTypes/dataTypesPractise.py
@@ -1,23 +1,23 @@
-# cost_bread = 3500
-# mijoz = int(input("How many bread do you want to buy ? "))
-# output = mijoz * cost_bread
+cost_bread = 3500
+mijoz = int(input("How many bread do you want to buy ? "))
+output = mijoz * cost_bread
 
-# print(output)
+print(output)
 
-# price = 100
-# mijoz = int(input("How much do you need ?"))
-# result = (price * mijoz)**2
-# print(result)
+price = 100
+mijoz = int(input("How much do you need ?"))
+result = (price * mijoz)**2
+print(result)
 
 
-# first_customer = int(input(" First: Enter your price ?"))
-# second_customer = int(input(" Second: Enter your price ?"))
+first_customer = int(input(" First: Enter your price ?"))
+second_customer = int(input(" Second: Enter your price ?"))
 
-# if first_customer > second_customer :
-#     print("Your profit", first_customer * 2)
-# elif first_customer < second_customer :
-#     print("Your profit", second_customer * 3)
-# else:print("You have to enter number")
+if first_customer > second_customer :
+    print("Your profit", first_customer * 2)
+elif first_customer < second_customer :
+    print("Your profit", second_customer * 3)
+else:print("You have to enter number")
 
 
 str = "pynative"
